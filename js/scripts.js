0d75010d9d3790ac902817c45650d9f2e76f257d
Amy <amyjohnsonmichelle@gmail.com>
2015-02-19 15:21:01 -0800

    Allow user to enter lists and tasks for each individual list

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,39 @@
+// var List = {name: "", tasks:[]};
+
+
 $(function() {
+  var currentList = {name: "", task: []}
+  // var currentList = Object.create(List);
+
   $("form#lists").submit(function(event) {
     event.preventDefault();
 
     var name = $("input#new-list").val();
-    var list = {name: name, tasks: []};
+    var newList = {name: name, tasks: []}
 
-    $("ul#to-do-lists").append("<li><span class='active-list'>"+ list.name + "</span></li>");
-
+    $("ul#to-do-lists").append("<li><span class='active-list'>"+ newList.name + "</span></li>");
 
     $("input#new-list").val("");
 
-    $("#to-do-lists li").last().click(function() {
-      $("#tasks").show()
-      $("ul#these-tasks").empty()
-      $("h2.this-list").text(list.name);
-    });
-
-    $("form#tasks").submit(function(event) {
-      event.preventDefault();
-      var task = $("input.new-task").val();
-      list.tasks.push(task);
-
-      list.tasks.forEach(function(task) {
+    $(".active-list").last().click(function() {
+      $("#tasks").show();
+      $("h2.this-list").text(newList.name);
+      $("ul#these-tasks").empty();
+      newList.tasks.forEach(function(task) {
         $("ul#these-tasks").append("<li>" + task + "</li>");
       });
+      currentList = newList;
+    });
+  });
+
+  $("form#tasks").submit(function(event) {
+    event.preventDefault();
+    var task = $("input.new-task").val();
+    currentList.tasks.push(task);
+    $("ul#these-tasks").empty();
+    currentList.tasks.forEach(function(task) {
+      $("ul#these-tasks").append("<li>" + task + "</li>");
+    $("input.new-task").val("");
     });
   });
 });
